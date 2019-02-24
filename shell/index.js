026717a6af0f4afc99113aef8746d75a82fd8d0e
Kevin Higgs <github@kevinhiggs.com>
2019-02-24 16:43:16 -0500

    path rewrite

--- shell/index.js
+++ shell/index.js
@@ -62,7 +62,7 @@ module.exports = function (ctf) {
 
 	globalRouter.use('/deploy', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
 		if (req.user.admin === true) {
-			proxy({ target: process.env.SHELL_DEPLOY_URL, changeOrigin: true, headers: { Authorization: process.env.SHELL_DEPLOY_AUTH }, prependPath: false })(req, res, next)
+			proxy({ target: process.env.SHELL_DEPLOY_URL, changeOrigin: true, headers: { Authorization: process.env.SHELL_DEPLOY_AUTH }, pathRewrite: {'^/shell/deploy' : ''} })(req, res, next)
 		} else {
 			res.sendStatus(403)
 		}
