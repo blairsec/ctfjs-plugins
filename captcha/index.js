defcc93b6ab4855e64d7a515e522fe38304d2347
Kevin Higgs <github@kevinhiggs.com>
2019-04-18 20:05:28 -0400

    params

--- captcha/index.js
+++ captcha/index.js
@@ -3,11 +3,11 @@ module.exports = function (ctf) {
 	var axios = require('axios')
 
 	ctf.before('createUser', async function (req) {
-		var r = await axios.post('https://www.google.com/recaptcha/api/siteverify', {
+		var r = await axios.post('https://www.google.com/recaptcha/api/siteverify', {}, { params: {
 			response: req.headers['captcha'],
 			secret: process.env.RECAPTCHA_SECRET
-		})
-		if (!r.success) throw "Invalid captcha"
+		}})
+		if (!r.data.success) throw "Invalid captcha"
 	})
 
 }
