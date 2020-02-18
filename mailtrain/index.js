c2b6a7d7ad4034397839e945f2317b8594488767
Kevin Higgs <github@kevinhiggs.com>
2020-02-18 18:58:31 -0500

    only on verify

--- mailtrain/index.js
+++ mailtrain/index.js
@@ -9,11 +9,4 @@ module.exports = function (ctf) {
 		})
 	})
 
-	ctf.after('modifyUser', async function (req, data) {
-		await axios.post(process.env.MAILTRAIN_URL + '/api/subscribe/' + process.env.MAILTRAIN_LIST + '?access_token=' + process.env.MAILTRAIN_TOKEN, {
-			EMAIL: data.user.email,
-			FIRST_NAME: data.user.username
-		})
-	})
-
 }
